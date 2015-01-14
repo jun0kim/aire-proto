6ec07cca449ccb78a4a65f765f595f1eaaa2f82b
Jun <serr92b@gmail.com>
2015-01-14 08:22:51 +0000

    fix

--- public/js/aire-2.js
+++ public/js/aire-2.js
@@ -1,57 +1,75 @@
 $(function () {
 	var inputElement = $('.sending-input');
 	var cancel = $('#nav-cancel');
+	var pay_currency = 'usd';
 	var balance = $('#current-balance').text();
 	var airepay_btn = $('#aire-pay > .actions');
-	var convert_inputElement = $('#convert input');
+	var pay_input_balance;
+	var currency_symbol = {
+		"usd":"$", "jpy":"¥", "gbp":"€", "mxn":"$", "cny":"¥", "krw":"₩", "euro":"€", "btc":"B"
+	}
 
 	inputElement.each(function () {
 		$(this).focus(function() {
 			$('#aire-pay').addClass('focused');
-		});
-
-		$(this).on("input", function() {
+		})
+		.on("input", function() {
 			valid_input();
 		});
-
-		$(this).on("blur", function() {
-			
-		});
 	});
-
-	convert_inputElement.each(function () {
-		$(this).focus(function() {
-			$('#convert').addClass('focused');
-		});
-
-		$(this).blur(function() {
-			$('#convert').removeClass('focused');
-		});
+	$('#input-friend').focus( function (){
+		$('#aire-pay').addClass('friends');
+	})
+	.blur(function() {
+		$('#aire-pay').removeClass('friends');
+		$('#pay-success .send-name').text($(this).val());
 	});
 
 	cancel.on("click", clear_input);
-	$('#pay-btn').click(clear_input);
+	$('#pay-btn').click(function (){
+		clear_input();
+		set_currency_pay_success();
+		set_saved_money();
+	});
 	$('#input-value').on("blur", function() {
 		balance_animation();
 	});
+
+	$('#aire-pay select').change(function (){
+		pay_currency = $(this).find('> option:selected').attr('val');
+		var symbol = $('#aire-pay-input input[type="text"][name="value"].usd + label');
+		symbol.removeClass();
+		symbol.addClass(pay_currency);
+	});
+
+	function set_saved_money() {
+		var money_str = currency_symbol[pay_currency] + (pay_input_balance/10).toFixed(1);
+		$('#pay-success .saved-money').text(money_str);
+	}
+
+	function set_currency_pay_success() {
+		$('#confirm-balance').removeClass();
+		$('#confirm-balance').addClass(pay_currency);
+	}
 
 	function clear_input(){
 		$('#aire-pay').removeClass('focused');
 		airepay_btn.removeClass("on");
 		inputElement.each(function (){
 			$(this).val('');
-			$('#aire-pay-balance').css("width","100%");
-			$('#remain-balance').text(balance);
 		});
+		$('#aire-pay-balance').css("width","100%");
+		$('#remain-balance').text(balance);
+		$('#aire-pay').removeClass('friends');
 	}
 
 	function balance_animation() {
-		var input_balance = $('#input-value').val();
-		var percent = (balance - input_balance) / balance;				
+		pay_input_balance = $('#input-value').val();
+		var percent = (balance - pay_input_balance) / balance;				
 		percent = percent * 100 +'%';
 		$('#aire-pay-balance').css("width",percent);
-		$('#remain-balance').text(balance-input_balance);
-		$('#confirm-balance').text(input_balance);
+		$('#remain-balance').text(balance-pay_input_balance);
+		$('#confirm-balance').text(pay_input_balance);
 	}
 
 	function valid_input() {
@@ -60,7 +78,6 @@ $(function () {
 			if( $(this).val().length > 0)
 				count++;
 		});
-		
 		if(count == 3){
 			airepay_btn.addClass("on");
 		}
@@ -68,18 +85,106 @@ $(function () {
 			airepay_btn.removeClass("on");
 		}
 	}
-	var selector = $('select[name=currency]');
 
-	selector.change(current_selector);
 
-	function current_selector() {
-		var index = selector.find('> option').index( selector.find('> option:selected'));
-		console.log(index);
-		console.log(selector.find('> option:selected'));
-		console.log($('select[name=currency] option:selected').text());
+
+// convert //
+	var convert_inputElement = $('#convert input');
+	var convert_balance = $('#convert .current-balance .money').text();
+	var currency_to = 'usd';
+	var currency_from ='euro';
+
+	var rate = {
+		"usd":1.18, "jpy":138.14, "gbp":0.78, "mxn":17.29, "cny":7.31, "krw":1275.89
+	}
+	
+
+	$('#convert .nav .cancel-btn').click(convert_clear_input);
+
+	convert_inputElement.each(function () {
+		$(this).focus(function() {
+			$('#convert').addClass('focused');
+		});
+	});
+
+	$('#convert .convert-input .source').on("blur", function() {
+		convert_animation();
+		calc_fee();
+	})
+	.on("input", calc_rate);
+
+	$('#convert select').change(function (){
+		currency_to = $(this).find('> option:selected').attr('val');
+		var symbol = $('#convert .convert-input input[name="target"] + label');
+		symbol.removeClass();
+		symbol.addClass(currency_to);
+		calc_rate();
+		change_rate_text();
+		change_rate_convert_success();
+		change_btn_to();
+	});
+
+	$('#convert-accept').click(convert_clear_input);
+
+	function change_btn_to() {
+		$('#convert .convert-btn .currency-to').text
+		(currency_to.toUpperCase());
+	}
+
+	function calc_fee() {
+		var total = $('#convert .convert-input .source').val();
+		var fee_str = currency_symbol['euro'] + ' ' + (total/100).toFixed(2);
+
+		$('#convert .fee').each(function() {
+			$(this).text(fee_str);
+		});
 
 	}
 
+	function change_rate_convert_success() {
+		var tar = $('#convert-balance-tar');
+		tar.removeClass();
+		tar.addClass(currency_to);
+	}
 
+	function set_val_convert_success(src, tar) {
+		$('#convert-balance-src').text(src);
+		$('#convert-balance-tar').text(tar);
+	}
+
+	function change_rate_text() {
+		var rate_str = rate[currency_to]+ ' '+currency_to.toUpperCase();
+		$('#convert .exchange-rate span').text(rate_str);
+	}
+
+	function calc_rate() {
+		var source = $('#convert .convert-input .source');
+		var target = $('#convert .convert-input .target');
+
+		target.val( (source.val() * rate[currency_to]).toFixed(2) );
+		set_val_convert_success(source.val(), target.val());
+	}
+
+	function convert_clear_input(){
+		$('#convert').removeClass('focused');
+		convert_inputElement.each(function (){
+			$(this).val('');
+		});
+		$('#convert .fee').each(function (){
+			$(this).text('');
+		});
+
+		$('#convert .current-balance').css("width","100%");
+		$('#convert-remain-balance').text(convert_balance);
+	}
+
+	function convert_animation() {
+		var input_balance = $('#convert .convert-input .source').val();
+		var percent = (convert_balance - input_balance) / convert_balance;				
+		percent = percent * 100 +'%';
+		$('#convert .current-balance').css("width",percent);
+		$('#convert-remain-balance').text( (convert_balance-input_balance).toFixed(2) );
+	}
+	
 });
 
